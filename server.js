838b3cd26b38d5c94f1d1d82104f0a2d849d8d79
Shashank <shashankbmusale07@gmail.com>
2025-01-18 16:54:32 +0530

    add pdf

--- server.js
+++ server.js
@@ -24,7 +24,10 @@ app.use((req, res, next) => {
 
 // Connect to MongoDB Atlas
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
